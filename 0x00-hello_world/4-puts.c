7aa8bbfa4967354bc0a712e04ec3b49a877837d3
olamide <moronkolaolamide@gmail.com>
2023-02-18 23:17:42 -0800

    mens

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/* 
+ * File: 4-puts.c
+ * Auth: Olamide O Moronkola
+ */
+
+#include <stdio.h>
+
+/* This is the main program that prints the out */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}
