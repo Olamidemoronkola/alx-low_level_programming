1f83a147518b32120092d171c06250ddc347aa4f
olamide <moronkolaolamide@gmail.com>
2023-02-18 23:32:48 -0800

    mens

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 /*
- * main - Prints "programming is like building a multilingual puzzle, followed by a new line.
- *
+ * main - Prints "programming is like building a multilingual puzzle,
+ * followed by a new line.
  * Return : Always 0.
  */
 int main(void)
