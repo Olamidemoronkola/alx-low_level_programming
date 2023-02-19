7f9894de5013ab6337ebf3bb016b7a7c18b9abc2
olamide <moronkolaolamide@gmail.com>
2023-02-19 00:00:08 -0800

    lol

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - entery way
+ *
+ * Description: 'writes a statement with printf'
+ * Returns 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
