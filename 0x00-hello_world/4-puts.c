005aad859cb9243f02799e2a2d917b55b5af6f93
olamide <moronkolaolamide@gmail.com>
2023-02-18 23:43:13 -0800

    mens

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
-/* 
- * File: 4-puts.c
- * Auth: Olamide O Moronkola
- */
-#include <stdio.h>
 /*
- * main - Prints "programming is like building a multilingual puzzle,
- * followed by a new line.
- * Return : Always 0.
+ * File: 4-puts.c
+ * Auth: Olamide O Moronkola <321@alx.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
