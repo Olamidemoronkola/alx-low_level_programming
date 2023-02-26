e9c3f7cd61ce39bcd8bfd5350263a484bba3722c
olamide <moronkolaolamide@gmail.com>
2023-02-25 16:48:38 -0800

    kok

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 /**
- * main - Prints all the numbers of base 16 in lowercase.
+ * main - Prints all the numbers of base sixteen in lowercase.
  *
- * Return: Always 0.
+ * Return: Always zero.
  */
 int main(void)
 {
