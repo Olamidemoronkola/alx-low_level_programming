8cda79c908cdb9271404bd0aa2dcd4908b17f2f3
olamide <moronkolaolamide@gmail.com>
2023-02-26 10:04:20 -0800

    this

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
