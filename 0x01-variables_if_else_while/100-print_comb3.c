5f2b20d3f8bad749a2d644c5109303cf21b96d39
olamide <moronkolaolamide@gmail.com>
2023-02-26 01:12:59 -0800

    It breaks my heart

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit, digit1;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit = 0; digit < 9; digit++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (digit1 = digit + 1; digit1 < 10; digit1++)
 		{
+			putchar((digit % 10) + '0');
 			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
+			if (digit == 8 && digit1 == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
