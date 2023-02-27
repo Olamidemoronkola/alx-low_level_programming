ea09a51b0f0649ea720ba7ac622ebb38fe71af39
olamide <moronkolaolamide@gmail.com>
2023-02-26 22:15:54 -0800

    i figured out the use of a header file and how to make and include it in your c code

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+/**
+ * This is a header file called main.h
+ *           main.h holds all the functions or things i can call
+ *           in the main function of other code as long as i include the file
+ *           using #include "header_filename.h" in the code
+ */
