1a559a149743fa6a1c24836be976852be0fa8da3
olamide <moronkolaolamide@gmail.com>
2023-02-28 22:21:32 -0800

    commmittt this nnnuttuts

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
  * _islower - this function checks for lowercase character
- * c: single letter input
+ * @c: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
