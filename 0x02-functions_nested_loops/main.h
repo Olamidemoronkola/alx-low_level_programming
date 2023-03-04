774c639791e6c577963868feb2aecd7083657371
olamide <moronkolaolamide@gmail.com>
2023-03-04 02:01:35 -0800

    ligma

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,6 +15,6 @@ int _abs(int);
 int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
-int add(int one, int two);
+int add(int x, int y);
 void print_to_98(int n);
 void print_times_table(int n);
